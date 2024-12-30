8905babea856c44f11284be8d1c05b6ac0d362b0
Joshua Pendragon <jpendragon@sorenson.com>
2024-12-29 20:39:04 -0600

    day 11 part 2 cleanup

--- src/days/day_eleven.rs
+++ src/days/day_eleven.rs
@@ -19,8 +19,7 @@ impl DayEleven {
         let start = Instant::now();
         let stones = parse_input(input);
         let result = blink_n_times(stones, blinks);
-        let duration = start.elapsed();
-        println!("Total time: {:?}", duration);
+        println!("Total time: {:?}", start.elapsed());
         result.to_string()
     }
 }
@@ -39,57 +38,43 @@ fn count_blinks(stone: Stone, depth: usize, cache: &mut Cache) -> usize {
         return value;
     }
     let (left, right) = process_stone(stone);
-    if depth == 1 {
+    let output = if depth == 1 {
         if right.is_none() {
-            cache.insert((stone, depth), 1);
             1
         } else {
-            cache.insert((stone, depth), 2);
             2
         }
     } else {
         let mut output = count_blinks(left.unwrap(), depth - 1, cache);
-        if right.is_some() {
-            output += count_blinks(right.unwrap(), depth - 1, cache);
+        if let Some(right) = right {
+            output += count_blinks(right, depth - 1, cache);
         }
-        cache.insert((stone, depth), output);
         output
-    }
+    };
+    cache.insert((stone, depth), output);
+    output
 }
 
 fn process_stone(stone: Stone) -> (Option<Stone>, Option<Stone>) {
-    let result = if stone == 0 {
+    if stone == 0 {
         (Some(1), None)
     } else if count_digits(stone) % 2 == 0 {
         let (left, right) = split_integer_into_halves(stone);
         (Some(left), Some(right))
     } else {
-        let new_stone = stone * 2024;
-        (Some(new_stone), None)
-    };
-    result
+        (Some(stone * 2024), None)
+    }
 }
 
 fn split_integer_into_halves(num: u64) -> (u64, u64) {
-    if num == 0 {
-        return (0, 0); // Special case for 0
-    }
-
-    let total_digits = count_digits(num);
-    let half_digits = total_digits / 2;
-
-    // Calculate the divisor for splitting the number
+    let half_digits = count_digits(num) / 2;
     let divisor = 10u64.pow(half_digits);
-
-    let first_half = num / divisor;
-    let second_half = num % divisor;
-
-    (first_half, second_half)
+    (num / divisor, num % divisor)
 }
 
 fn count_digits(num: u64) -> u32 {
     if num == 0 {
-        1 // Special case for 0, which has 1 digit
+        1
     } else {
         (num as f64).log10().floor() as u32 + 1
     }
